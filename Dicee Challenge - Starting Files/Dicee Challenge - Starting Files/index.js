ef67e86da541a5817887693aa1259c64d130e85c
harshkanani204 <harshkanani204@gmail.com>
2023-06-25 14:42:48 +0530

    Completed Dice challenge

---
+++ Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var img1 = "images/dice"+randomNumber1 + ".png";
+var img2 = "images/dice"+randomNumber2 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",img1);
+document.querySelectorAll("img")[1].setAttribute("src",img2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins!!!"
+}
+
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Wins!!!"
+}
+
+else{
+    document.querySelector("h1").innerHTML="Draw!!!"
+}
