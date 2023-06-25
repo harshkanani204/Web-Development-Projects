44709726a0001dbc5b30792815fd508a6c93e7c3
harshkanani204 <harshkanani204@gmail.com>
2023-06-25 14:18:03 +0530

    Completed DOM

---
+++ DOM Challenge Starting Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("li")[2].style.color = "purple";
+document.querySelector("button").style.backgroundColor="yellow";
+
+document.querySelector("h1").classList.add("huge"); 
