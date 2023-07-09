63d121a19f11e1a5caba83ea01c86c7a126492b4
harshkanani204 <harshkanani204@gmail.com>
2023-07-09 19:56:36 +0530

    Completed Weather App

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req,res){
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=a284058456d011022d2d029ca38f1003";
+    res.sendFile(__dirname+"/index.html");
+    
+});
+
+app.post("/", function(req,res){
+
+    const query = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=a284058456d011022d2d029ca38f1003";
     
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -14,12 +24,15 @@ app.get("/", function(req,res){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const des = weatherData.weather[0].description;
-            console.log(temp,des);
+            const icon = weatherData.weather[0].icon;
+            const iconURL = "https://openweathermap.org/img/wn/"+ icon + "@2x.png";
+            res.write("<p>The weather is currently "+ des + "</p>");
+            res.write("<h1>The temperature is " + temp + " Kelvin </h1>");
+            res.write("<img src='" + iconURL+ "'/>");
+            res.send();
         });
 
     });
-
-    res.send("This is the home page");
 });
 
 
