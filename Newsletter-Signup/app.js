353948988f8d55883bcbade4c36aae8670b9c122
harshkanani204 <harshkanani204@gmail.com>
2023-07-11 14:42:39 +0530

    Completed API

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -15,9 +16,62 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [
+            {
+                email_address: email, 
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us12.api.mailchimp.com/3.0/lists/543a056e55";
+
+    const options ={
+        method: "POST",
+        auth: "harshkanani204:6267ff5ffd93676a26ec807cc86b55d2-us12"
+    }
+
+    const request = https.request(url,options, function(response){
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        }
+
+        else
+        {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data));
+        });
+
+    });
+
+    request.write(jsonData);
+    request.end();
+
 });
+
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+});
+
+// API-key
+// 6267ff5ffd93676a26ec807cc86b55d2-us12
+
+// List ID 
+// 543a056e55
